89f64802f50ab5f4e4bfcc3d958cd6449033e2a0
Ethan Law <ejlaw01@gmail.com>
2017-02-17 15:09:09 -0800

    gallery will take and display annotations at full size image

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -9,10 +9,32 @@ import { Encounter } from '../encounter';
 
 export class GalleryComponent implements OnInit {
 
+  @Input() encounters: Encounter[];
+
+  getHeight() {
+    var height = this.encounters[0].mediaAssets[0].annotation.height + "px";
+    return height;
+  }
+
+  getWidth() {
+    var width = this.encounters[0].mediaAssets[0].annotation.width + "px";
+    return width;
+  }
+
+  getLeft(){
+    var left = this.encounters[0].mediaAssets[0].annotation.x + "px";
+    return left;
+  }
+
+  getTop() {
+    var top = this.encounters[0].mediaAssets[0].annotation.y + "px";
+    return top;
+  }
+
+
   constructor() { }
 
   ngOnInit() {  }
 
-  @Input() encounters: Encounter[];
 
 }
